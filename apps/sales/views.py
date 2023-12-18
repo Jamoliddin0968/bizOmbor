dd227f6051deb78e17e65ac463b9b2d4467b3595
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-12-18 20:01:31 +0500

    add sale

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -8,9 +8,9 @@ class SaleCreateAPIView(CreateAPIView):
     queryset = Sale.objects.all()
     serializer_class = SaleCreateSerializer
 
-    @extend_schema(
-        request=SaleCreateSerializer,
-        responses={201:SaleSerializer },
-    )
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    # @extend_schema(
+    #     request=SaleCreateSerializer,
+    #     responses={201:SaleSerializer },
+    # )
+    # def post(self, request, *args, **kwargs):
+    #     return self.create(request, *args, **kwargs)
