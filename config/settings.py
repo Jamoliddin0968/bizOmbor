9130813f9d8fd322b294615e9c71e2b0eeb50cc3
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-12-25 17:55:14 +0500

    DFgh

--- config/settings.py
+++ config/settings.py
@@ -100,19 +100,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ware',
-        'USER': 'root',
-        'PASSWORD': '12345678',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'ware',
+    #     'USER': 'root',
+    #     'PASSWORD': '12345678',
+    #     'HOST':'localhost',
+    #     'PORT':'3306',
+    # }
 
 }
 
