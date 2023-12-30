bdd70b4c32f23878b594adad8bebd037af5e2a5c
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-12-30 19:41:17 +0500

    add store

--- apps/products/filters.py
+++ apps/products/filters.py
@@ -1,9 +1,10 @@
 from django_filters import rest_framework as filters
 from .models import Product
 class ProductFilter(filters.FilterSet):
+    store_id = filters.NumberFilter()
     barcode = filters.CharFilter(min_length=1)
     barcode_type = filters.CharFilter(min_length=1)
 
     class Meta:
         model = Product
-        fields = ['barcode', 'barcode_type']
+        fields = ['barcode', 'barcode_type','store']
