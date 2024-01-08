207ad5ca986d29bf422456c4e42f1e1e49b907ff
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2024-01-08 19:36:45 +0500

    add user

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,24 +1,34 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
+from .forms import UserChangeForm, UserCreationForm
 from .models import User
 
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ('username', 'is_manager')
+    list_filter = ('is_manager',)
 
-@admin.register(User)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ("username", "is_staff", "is_active",'is_manager')
-    list_filter = ("username", "is_staff", "is_active",'is_manager')
     fieldsets = (
-        (None, {"fields": ("username",'password','is_manager','first_name','last_name')}),
+        (None, {'fields': ('username', 'password', 'first_name', 'last_name','is_manager')}),
+        # ('Permissions', {'fields': ('is_admin',)}),
     )
+
     add_fieldsets = (
         (None, {
-            "classes": ("wide",),
-            "fields": (
-                "username", "password",'is_manager'
-            )}
-        ),
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
     )
-    search_fields = ("username",)
-    ordering = ("username",)
-    readonly_fields = ('password',)
+
+    search_fields = ('username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+    class Meta:
+        model = User
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
