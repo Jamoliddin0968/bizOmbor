8d76dd14f012043e173c8f1776de2905a3325bbc
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2024-01-08 19:06:23 +0500

    df

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register([User])
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "is_staff", "is_active",)
+    list_filter = ("username", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username", "password"
+            )}
+        ),
+    )
+    search_fields = ("username",)
+    ordering = ("username",)
--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,8 @@
 import os.path
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,22 +101,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
-
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'ware',
-    #     'USER': 'root',
-    #     'PASSWORD': '12345678',
-    #     'HOST':'localhost',
-    #     'PORT':'3306',
-    # }
-
 }
+
+
 
 
 # Password validation
