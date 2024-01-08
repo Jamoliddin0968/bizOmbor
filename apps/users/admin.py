240aa5c5cef1be404534e6dbfda79e3f098b84be
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2024-01-08 19:24:14 +0500

    fdg

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -6,18 +6,19 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ("username", "is_staff", "is_active",)
-    list_filter = ("username", "is_staff", "is_active",)
+    list_display = ("username", "is_staff", "is_active",'is_manager')
+    list_filter = ("username", "is_staff", "is_active",'is_manager')
     fieldsets = (
-        (None, {"fields": ("username", "password")}),
+        (None, {"fields": ("username",'password','is_manager','first_name','last_name')}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
             "fields": (
-                "username", "password"
+                "username", "password",'is_manager'
             )}
         ),
     )
     search_fields = ("username",)
     ordering = ("username",)
+    readonly_fields = ('password',)
