0f11402acf72e5e5daad5ae62fe3ee9bd3cb3285
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-06-12 22:08:06 +0500

    yangi xatolar o'g'naldi

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import LoginManager, LoginWorker
+# from .views import LoginManager, LoginWorker
 
 urlpatterns = [
     # path('manager/', LoginManager.as_view(), name='login+for_manager'),
