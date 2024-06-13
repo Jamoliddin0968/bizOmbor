59ec9baab32c113b6d95c01f55d5890283b8d87d
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-06-14 00:09:32 +0500

    add new pi req.txt

--- config/settings.py
+++ config/settings.py
@@ -100,14 +100,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
-    'mysql': {
-        'ENGINE': config('DB_ENGINE'),
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT"),
-    },
+    # 'mysql': {
+    #     'ENGINE': config('DB_ENGINE'),
+    #     'NAME': config("DB_NAME"),
+    #     'USER': config("DB_USER"),
+    #     'PASSWORD': config("DB_PASSWORD"),
+    #     'HOST': config("DB_HOST"),
+    #     'PORT': config("DB_PORT"),
+    # },
     
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
