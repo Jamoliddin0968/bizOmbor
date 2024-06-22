73284489789893e9b92b10388bba65500aaa88e8
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-06-22 23:43:47 +0500

    product ni versiya bo'yicha get qilish qo'shildi

--- apps/versions/urls.py
+++ apps/versions/urls.py
@@ -6,6 +6,7 @@ from .views import VersionProductListApiView, VersionViewSet
 router = DefaultRouter()
 router.register('versions', VersionViewSet, basename='version')
 urlpatterns = [
-    path('versions/<int:version_id>', VersionProductListApiView.as_view()),
+    path('versions/<int:version_id>/products',
+         VersionProductListApiView.as_view()),
     path('', include(router.urls)),
 ]
--- apps/versions/views.py
+++ apps/versions/views.py
@@ -30,19 +30,6 @@ class VersionViewSet(GenericViewSet):
         else:
             return Response({"error": "version_id is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=['get'], url_path='new_versions/(?P<version_id>[^/.]+)')
-    def get_new_versions(self, request, version_id=None):
-        if version_id is not None:
-            try:
-                version_id = int(version_id)
-                versions = Version.objects.filter(id__gt=version_id)
-                serializer = VersionSerializer(versions, many=True)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except ValueError:
-                return Response({"error": "Invalid version_id"}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({"error": "version_id is required"}, status=status.HTTP_400_BAD_REQUEST)
-
 
 class VersionProductListApiView(ListAPIView):
     serializer_class = ProductSerializer
